6196ad0bfc1806e77a935c09569c73a60925f18e
Víctor Castrillo Redondo <castrilloredondovictor@gmail.com>
2025-02-20 13:44:37 +0100

    Add project files.

---
+++ ProyectoBackPeluqueria/Controllers/ReservasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoBackPeluqueria.Controllers
+{
+    public class ReservasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
