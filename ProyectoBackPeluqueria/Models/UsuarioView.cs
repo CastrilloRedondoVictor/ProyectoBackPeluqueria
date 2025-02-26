7e6e17a928a43fcecc15974b39f7519703ce2284
Víctor Castrillo Redondo <castrilloredondovictor@gmail.com>
2025-02-26 12:32:43 +0100

    funciones servicio para perfil cliente

---
+++ ProyectoBackPeluqueria/Models/UsuarioView.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoBackPeluqueria.Models
+{
+    [Table("Vista_Usuarios")]
+    public class UsuarioView
+    {
+        [Key]
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Column("Nombre")]
+        public string Nombre { get; set; }
+
+        [Column("Apellidos")]
+        public string Apellidos { get; set; }
+
+        [Column("Telefono")]
+        public string Telefono { get; set; }
+
+        [Column("Email")]
+        public string Email { get; set; }
+
+        [Column("ColorPelo")]
+        public string ColorPelo { get; set; }
+
+        [Column("Imagen")]
+        public string Imagen { get; set; }
+
+        [Column("Rol")]
+        public string Rol { get; set; }
+    }
+}
