db0ccf56b761528bf29fd67cf7ad7ad4fbf29584
CastrilloRedondoVictor <castrilloredondovictor@gmail.com>
2025-04-01 14:24:46 +0200

    Removed some functions

--- ProyectoBackPeluqueria/Services/ServiceStorageBlobs.cs
+++ ProyectoBackPeluqueria/Services/ServiceStorageBlobs.cs
@@ -13,46 +13,6 @@ namespace ProyectoBackPeluqueria.Services
             _blobServiceClient = blobServiceClient;
         }
 
-        public async Task<List<string>> GetContainersAsync()
-        {
-            List<string> containers = new List<string>();
-            await foreach (BlobContainerItem container in _blobServiceClient.GetBlobContainersAsync())
-            {
-                containers.Add(container.Name);
-            }
-            return containers;
-        }
-
-        public async Task CreateContainerAsync(string containerName)
-        {
-            BlobContainerClient containerClient = _blobServiceClient.GetBlobContainerClient(containerName);
-            await containerClient.CreateIfNotExistsAsync();
-        }
-
-        public async Task DeleteContainerAsync(string containerName)
-        {
-            BlobContainerClient containerClient = _blobServiceClient.GetBlobContainerClient(containerName);
-            await containerClient.DeleteIfExistsAsync();
-        }
-
-
-        public async Task<List<BlobModel>> GetBlobsAsync(string containerName)
-        {
-            BlobContainerClient containerClient = _blobServiceClient.GetBlobContainerClient(containerName);
-            List<BlobModel> blobs = new List<BlobModel>();
-            await foreach (BlobItem blob in containerClient.GetBlobsAsync())
-            {
-                BlobModel model = new BlobModel
-                {
-                    Nombre = blob.Name,
-                    Url = $"{containerClient.Uri}/{blob.Name}",
-                    Container = containerName
-                };
-                blobs.Add(model);
-            }
-            return blobs;
-        }
-
         public async Task DeleteBlobAsync(string containerName, string blobName)
         {
             BlobContainerClient containerClient = _blobServiceClient.GetBlobContainerClient(containerName);
